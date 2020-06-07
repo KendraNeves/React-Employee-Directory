d9a9f02e339e2fc675a0c36670a596cfa9a93fff
Kendra Neves <kendraneves@gmail.com>
2020-06-07 17:11:08 -0400

    Updates

---
+++ precache-manifest.5eedcd38b516368f5c5242c3a212b9c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1a5b152791d6a39cdaefdcc289bbb0",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "d3d0e98d9dc084d61d54",
+    "url": "/React-Employee-Directory/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d79cf95a2598ef20c7d8",
+    "url": "/React-Employee-Directory/static/css/main.34919416.chunk.css"
+  },
+  {
+    "revision": "d3d0e98d9dc084d61d54",
+    "url": "/React-Employee-Directory/static/js/2.ac825e88.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/React-Employee-Directory/static/js/2.ac825e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79cf95a2598ef20c7d8",
+    "url": "/React-Employee-Directory/static/js/main.5d659059.chunk.js"
+  },
+  {
+    "revision": "9bd509428cee10405d35",
+    "url": "/React-Employee-Directory/static/js/runtime-main.9e2f96fa.js"
+  },
+  {
+    "revision": "0266ddc57f3d795a808104521459867b",
+    "url": "/React-Employee-Directory/static/media/yellow-pasta-spaghetti-eat-42326.0266ddc5.jpg"
+  }
+]);
